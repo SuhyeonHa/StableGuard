b348f77bf59e977f61fce1c52d7872eee83bedb2
SuhyeonHa <anguoyuan111@gmail.com>
2025-10-02 16:29:31 +0900

    fix: Use repository name instead of workspace

--- kubernetes/entrypoint.sh
+++ kubernetes/entrypoint.sh
@@ -10,16 +10,14 @@ set -e
 GIT_REPO="StableGuard"
 GIT_BRANCH="main"
 TOKEN_FILE="/mnt/nas5/suhyeon/tokens/github_token.txt"
-WORKSPACE = "/workspace"
 
 export GITHUB_TOKEN=$(cat "$TOKEN_FILE")
 
 GIT_REPO_URL="https://${GITHUB_TOKEN}@github.com/SuhyeonHa/${GIT_REPO}.git"
 
-rm -rf "${CLONE_DIR}"
-mkdir -p "${CLONE_DIR}"
-git clone --branch "${GIT_BRANCH}" "${GIT_REPO_URL}" "${CLONE_DIR}"
-cd "${CLONE_DIR}"
+rm -rf "${GIT_REPO}"
+git clone --branch "${GIT_BRANCH}" "${GIT_REPO_URL}"
+cd "${GIT_REPO}"
 echo "Clone complete."
 
 python eval_AGE.py
