dfc954b9bd93c40bc5508efb364ed2614134634c
SuhyeonHa <suhyeon.ha@kaist.ac.kr>
2025-10-11 22:44:52 +0900

    fix: Fix typos in train.sh

--- train.sh
+++ train.sh
@@ -1,6 +1,6 @@
  #!/bin/bash
  # Tips: You should replace data_root_path with your local coco dataset path
- CUDA_VISIBLE_DEVICES=0,1 accelerate launch --config_file /app/accelerate_config.yaml train.py \
+ CUDA_VISIBLE_DEVICES=0,1 accelerate launch --config_file accelerate_config.yaml train.py \
   --pretrained_model_name_or_path="stabilityai/stable-diffusion-2-1-base" \
   --data_root_path="/mnt/nas5/suhyeon/datasets/coco-2017" \
   --mask_pool_path="/mnt/nas5/suhyeon/datasets/coco-tamper-stableguard/datasets/mask_pool" \
@@ -12,7 +12,6 @@
   --output_dir="/mnt/nas5/suhyeon/projects/freqloc-ldm/exp1" \
   --save_steps=10000 \
   --num_train_epochs=10 \
-  --num_bits=48 \ 
   --noise_strength 0.0 0.8 \
   --cache_dir="/mnt/nas5/suhyeon/caches" \
   --watermark_size=32
