b0c6cbc117c0d5c226d295e5a09295b2adc3cb76
SuhyeonHa <suhyeon.ha@kaist.ac.kr>
2025-10-13 17:24:48 +0900

    chore: Increase # gpus

--- train.sh
+++ train.sh
@@ -1,6 +1,6 @@
  #!/bin/bash
  # Tips: You should replace data_root_path with your local coco dataset path
- CUDA_VISIBLE_DEVICES=0,1 accelerate launch --config_file accelerate_config.yaml train.py \
+ CUDA_VISIBLE_DEVICES=0,1,2,3 accelerate launch --config_file accelerate_config.yaml train.py \
   --pretrained_model_name_or_path="stabilityai/stable-diffusion-2-1-base" \
   --data_root_path="/mnt/nas5/suhyeon/datasets/coco-2017" \
   --mask_pool_path="/mnt/nas5/suhyeon/datasets/coco-tamper-stableguard/datasets/mask_pool" \
