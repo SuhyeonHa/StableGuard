3e252517078e7de723313a7958bd20364de2a0a1
SuhyeonHa <suhyeon.ha@kaist.ac.kr>
2025-10-14 15:15:04 +0900

    fix: Change exp name

--- train.sh
+++ train.sh
@@ -9,7 +9,7 @@
   --dataloader_num_workers=4 \
   --learning_rate=1e-4 \
   --weight_decay=1e-2 \
-  --output_dir="/mnt/nas5/suhyeon/projects/freqloc-ldm/exp03-add-vae-noise" \
+  --output_dir="/mnt/nas5/suhyeon/projects/freqloc-ldm/exp04-incr-vae-noise" \
   --save_steps=10000 \
   --num_train_epochs=10 \
   --noise_strength 0.0 0.3 \
