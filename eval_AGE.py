856770775a080097d3cfa21a6fd1b46e1e41c0a7
SuhyeonHa <suhyeon.ha@kaist.ac.kr>
2025-10-15 13:29:27 +0900

    fix: Embedding omniguard watermark in 512 (official traininig size)

--- eval_AGE.py
+++ eval_AGE.py
@@ -292,7 +292,11 @@ def generate_watermark_image(norm, weight_path, target_model, src_image_path, sa
             cover_input = dwt((images + 1.0) / 2.0) # [-1, 1] to [0, 1]
             secret_input = dwt(secret)
             msgs = torch.randint(2, (1, 64)).to(torch.float32).cuda()
-
+            
+            # omniguard is trained on 512x512 images
+            cover_inputs = F.interpolate(cover_input, size=(512, 512), mode="bilinear", align_corners=False)
+            secret_inputs = F.interpolate(secret_input, size=(512, 512), mode="bilinear", align_corners=False)
+            
             cover_images, output_z, out_temp, secret_temp = net(cover_input, secret_input, msgs)
             cover_images = cover_images * 2.0 - 1.0 # [-1, 1]
 
@@ -586,8 +590,8 @@ if __name__ == "__main__":
     # ------------------ Configuration ------------------
     run_config = {
         'src_image_path': "/mnt/nas5/suhyeon/datasets/valAGE-Set",
-        'target_model': "wam", # ["omniguard", "wam", "stableguard"]
-        'save_path': "/mnt/nas5/suhyeon/projects/eval_spliceless/wam/256_valAGE_sd",
+        'target_model': "omniguard", # ["omniguard", "wam", "stableguard"]
+        'save_path': "/mnt/nas5/suhyeon/projects/eval_spliceless/omniguard/256_valAGE_sd",
         'edit_model_name': "sd-legacy/stable-diffusion-inpainting",
         'size': 256,
     }
