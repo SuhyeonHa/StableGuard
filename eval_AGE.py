f7aafe522d794c339266246b27391a5462dce373
SuhyeonHa <suhyeon.ha@kaist.ac.kr>
2025-10-15 13:50:14 +0900

    fix: Change configures for stableguard

--- eval_AGE.py
+++ eval_AGE.py
@@ -591,8 +591,8 @@ if __name__ == "__main__":
     # ------------------ Configuration ------------------
     run_config = {
         'src_image_path': "/mnt/nas5/suhyeon/datasets/valAGE-Set",
-        'target_model': "omniguard", # ["omniguard", "wam", "stableguard"]
-        'save_path': "/mnt/nas5/suhyeon/projects/eval_spliceless/omniguard/256_valAGE_sd",
+        'target_model': "stableguard", # ["omniguard", "wam", "stableguard"]
+        'save_path': "/mnt/nas5/suhyeon/projects/eval_spliceless/stableguard/256_valAGE_sd",
         'edit_model_name': "sd-legacy/stable-diffusion-inpainting",
         'size': 256,
     }
