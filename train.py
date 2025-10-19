0036136323353946df9b297f25d4393b7ce859c5
SuhyeonHa <suhyeon.ha@kaist.ac.kr>
2025-10-19 21:11:17 +0900

    fix: Visualize both tamper and tamper noisy images

--- train.py
+++ train.py
@@ -406,6 +406,8 @@ def train_one_epoch(args, epoch, accelerator, train_dataloader, weight_dtype, mp
                                                cover_images[:args.train_batch_size], 
                                                ((cover_images - decode_images) *10)[:args.train_batch_size],
                                                random_masks.repeat(1, 3, 1, 1)[:args.train_batch_size], 
+                                               tamper_images[:args.train_batch_size],
+                                               tamper_noisy_images[:args.train_batch_size],
                                                F.sigmoid(F.interpolate(pred_mask, (args.resolution, args.resolution))).repeat(1, 3, 1, 1)[:args.train_batch_size],
                                                F.sigmoid(F.interpolate(pred_noisy_mask, (args.resolution, args.resolution))).repeat(1, 3, 1, 1)[:args.train_batch_size]],
                                                dim=0).detach().clone()
