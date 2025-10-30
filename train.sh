bef1d4f73a30109fbb5fe2a55c6402a020d33e9d
SuhyeonHa <suhyeon.ha@kaist.ac.kr>
2025-10-30 22:16:55 +0900

    fix: Change GPU

--- train.sh
+++ train.sh
@@ -5,7 +5,7 @@
   --mask_pool_path="/mnt/nas5/suhyeon/datasets/coco-tamper-stableguard/datasets/mask_pool" \
   --resolution=256 \
   --train_batch_size=8 \
-  --dataloader_num_workers=1 \
+  --dataloader_num_workers=4 \
   --learning_rate=1e-4 \
   --weight_decay=1e-2 \
   --output_dir="/mnt/nas5/suhyeon/projects/freqloc-ldm/exp16-baseline-train-decoder-bit" \
