e96475319a4a72453dc73a94002121e89bc1921b
SuhyeonHa <suhyeon.ha@kaist.ac.kr>
2025-11-02 22:15:38 +0900

    exp: Train pretrained decoder with gpus

--- train.py
+++ train.py
@@ -193,11 +193,11 @@ def main():
     moe_gfn = MoEGuidedForensicNet()
     lpips = LPIPS(net="vgg") # WatsonDistanceVgg() both Perceptual loss is ok, WatsonDistanceVgg can get better image quality
 
-    # for name, param in original_vae.decoder.named_parameters():
-    #     if name in mpw_vae_decoder.state_dict():
-    #         mpw_vae_decoder.state_dict()[name].copy_(param.detach().clone())
-    #     else:
-    #         print(name)
+    for name, param in original_vae.decoder.named_parameters():
+        if name in mpw_vae_decoder.state_dict():
+            mpw_vae_decoder.state_dict()[name].copy_(param.detach().clone())
+        else:
+            print(name)
 
     # freeze parameters of models to save more memory
     original_vae.requires_grad_(False)
@@ -343,8 +343,8 @@ def train_one_epoch(args, epoch, accelerator, train_dataloader, weight_dtype, mp
 
             # Loss
             # similarity loss
-            lpips_loss = lpips(cover_images, images.float().detach().clone()).mean() # 0.1
-            mae_loss = F.l1_loss(cover_images, images.float().detach().clone()) # 0.1
+            lpips_loss = 0.1 * lpips(cover_images, images.float().detach().clone()).mean() # 0.1
+            mae_loss = 0.1 * F.l1_loss(cover_images, images.float().detach().clone()) # 0.1
 
             # watermark loss
             # msg_loss = F.binary_cross_entropy_with_logits(pred_msgs, msgs.float().detach().clone())
@@ -363,7 +363,7 @@ def train_one_epoch(args, epoch, accelerator, train_dataloader, weight_dtype, mp
             #     loss = mae_loss + lpips_loss + mask_loss
             # else:
             #     loss = mae_loss + lpips_loss + mask_loss + noisy_mask_loss
-            loss = mae_loss + lpips_loss + 0.5 * mask_loss + 0.5 * noisy_mask_loss
+            loss = mae_loss + lpips_loss + mask_loss + noisy_mask_loss
 
             # for bit acc
             # pred_msgs_bin = torch.round(torch.sigmoid(pred_msgs))
--- train.sh
+++ train.sh
@@ -8,7 +8,7 @@
   --dataloader_num_workers=4 \
   --learning_rate=1e-4 \
   --weight_decay=1e-2 \
-  --output_dir="/mnt/nas5/suhyeon/projects/freqloc-ldm/exp17-baseline-train-decoder-scratch" \
+  --output_dir="/mnt/nas5/suhyeon/projects/freqloc-ldm/exp18-baseline-train-decoder-gpus" \
   --save_steps=1000 \
   --num_train_epochs=10 \
   --num_bits=48 \
