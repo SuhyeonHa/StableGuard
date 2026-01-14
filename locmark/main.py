c6b6b2d4d3f1ad8d1767f408b8874888cc5439e4
SuhyeonHa <suhyeon.ha@kaist.ac.kr>
2026-01-14 15:14:11 +0900

    chore: fix for kube

--- locmark/main.py
+++ locmark/main.py
@@ -28,9 +28,9 @@ class Params:
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         self.train_datasets = '/mnt/nas5/suhyeon/datasets/valAGE-Set'
         self.image_path = '/mnt/nas5/suhyeon/datasets/valAGE-Set/0034.png'
-        self.exp_name = 'masked_noise'
+        self.exp_name = 'pgd-8-255' # NOTE
         # self.output_dir = f'/mnt/nas5/suhyeon/projects/locmark/{self.exp_name}' # single image optimization
-        self.output_dir = f'/mnt/nas5/suhyeon/projects/eval_spliceless/ours_ones' # NOTE: multi image optimization, exp_name
+        self.output_dir = f'/mnt/nas5/suhyeon/projects/eval_spliceless/ours_full' # NOTE: multi image optimization, exp_name
         # self.output_dir = "/mnt/nas5/suhyeon/projects/locmark/" # single image optimization
         self.single_image_mode = False # NOTE
         self.num_test_images = 100 # the first n images
