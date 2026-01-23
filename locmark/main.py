b397346cc264d514d2917f792f538281bd64f37e
SuhyeonHa <suhyeon.ha@kaist.ac.kr>
2026-01-23 23:12:10 +0900

    exp: Add L2 norm constraints on perturbations

--- locmark/main.py
+++ locmark/main.py
@@ -28,7 +28,7 @@ class Params:
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         self.train_datasets = '/mnt/nas5/suhyeon/datasets/valAGE-Set'
         self.image_path = '/mnt/nas5/suhyeon/datasets/valAGE-Set/0034.png'
-        self.exp_name = 'pgd-l1-13500' # NOTE
+        self.exp_name = 'pgd-l2-20' # NOTE
         # self.output_dir = f'/mnt/nas5/suhyeon/projects/locmark/{self.exp_name}' # single image optimization
         self.output_dir = f'/mnt/nas5/suhyeon/projects/eval_spliceless/ours_full' # NOTE: multi image optimization, exp_name
         # self.output_dir = "/mnt/nas5/suhyeon/projects/locmark/" # single image optimization
@@ -64,7 +64,7 @@ class Params:
         self.lambda_p = 0.1 #0.05 #0.025
         self.lambda_i = 0.05 #0.01 #0.005
         self.feat_layer = 1
-        self.epsilon = 13500
+        self.epsilon = 20
 
         # --- Robustness Parameters --- 
         self.eps0_std = [0.0, 0.25] # Latent noise
