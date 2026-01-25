1297167b2b3683c4c7c3ed79eb3382c0f65a7b09
SuhyeonHa <suhyeon.ha@kaist.ac.kr>
2026-01-25 16:49:18 +0900

    exp: Increase lr and epsilon

--- locmark/main.py
+++ locmark/main.py
@@ -59,12 +59,12 @@ class Params:
         self.target_cossim = 0.2
 
         # --- Optimization Parameters ---
-        self.lr = 0.01  # Step size for L∞ PGD (Option C: α ≈ 2.5×ε/T)
+        self.lr = 0.05  # Step size for L∞ PGD (Option C: α ≈ 2.5×ε/T)
         self.steps = 300
         self.lambda_p = 0.001 #0.05 #0.025
         self.lambda_i = 0.001 #0.01 #0.005
         self.feat_layer = 1
-        self.epsilon = 0.3  # L∞ bound in latent space
+        self.epsilon = 1.0  # L∞ bound in latent space
 
         # --- Robustness Parameters --- 
         self.eps0_std = [0.0, 0.25] # Latent noise
