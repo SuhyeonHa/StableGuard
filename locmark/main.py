384bdecfc3997709bd555c3afbcbdb598ebf70a4
SuhyeonHa <suhyeon.ha@kaist.ac.kr>
2026-01-25 16:35:34 +0900

    exp: Set epsilon as 0.3

--- locmark/main.py
+++ locmark/main.py
@@ -28,7 +28,7 @@ class Params:
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         self.train_datasets = '/mnt/nas5/suhyeon/datasets/valAGE-Set'
         self.image_path = '/mnt/nas5/suhyeon/datasets/valAGE-Set/0034.png'
-        self.exp_name = 'pgd-linf-0.1' # NOTE: L∞ PGD with ε=0.1
+        self.exp_name = 'pgd-linf-0.3' # NOTE: L∞ PGD with ε=0.1
         # self.output_dir = f'/mnt/nas5/suhyeon/projects/locmark/{self.exp_name}' # single image optimization
         self.output_dir = f'/mnt/nas5/suhyeon/projects/eval_spliceless/ours_full' # NOTE: multi image optimization, exp_name
         # self.output_dir = "/mnt/nas5/suhyeon/projects/locmark/" # single image optimization
