10545e870b3c7537532e703264efe83ba223f563
SuhyeonHa <suhyeon.ha@kaist.ac.kr>
2026-02-11 13:32:55 +0900

    exp: Adjust loss weights to 0.025 and 0.005

--- locmark/main.py
+++ locmark/main.py
@@ -28,7 +28,7 @@ class Params:
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         self.train_datasets = '/mnt/nas5/suhyeon/datasets/valAGE-Set'
         self.image_path = '/mnt/nas5/suhyeon/datasets/valAGE-Set/0034.png'
-        self.exp_name = 'baseline'
+        self.exp_name = 'baseline-exp1'
         # self.output_dir = f'/mnt/nas5/suhyeon/projects/locmark/{self.exp_name}' # single image optimization
         self.output_dir = f'/mnt/nas5/suhyeon/projects/eval_spliceless/ours_ablation' # NOTE: multi image optimization, exp_name
         # self.output_dir = "/mnt/nas5/suhyeon/projects/locmark/" # single image optimization
@@ -61,8 +61,8 @@ class Params:
         # --- Optimization Parameters ---
         self.lr = 2.0
         self.steps = 300
-        self.lambda_p = 0.1 #0.05 #0.025
-        self.lambda_i = 0.05 #0.01 #0.005
+        self.lambda_p = 0.025 #0.1 #0.05 #0.025
+        self.lambda_i = 0.005 #0.05 #0.01 #0.005
         self.lambda_clean = 1.0
         self.lambda_noisy = 1.0
         self.feat_layer = 1
