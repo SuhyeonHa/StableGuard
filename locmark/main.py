4227a99a376f883ea1f5ea246c6aad75c6715829
SuhyeonHa <suhyeon.ha@kaist.ac.kr>
2026-02-19 23:45:56 +0900

    Train noise hard

--- locmark/main.py
+++ locmark/main.py
@@ -28,7 +28,7 @@ class Params:
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         self.train_datasets = '/mnt/nas5/suhyeon/datasets/valAGE-Set'
         self.image_path = '/mnt/nas5/suhyeon/datasets/valAGE-Set/0034.png'
-        self.exp_name = 'baseline-l0-p0.1'
+        self.exp_name = 'noise-hard-l0-p0.1'
         # self.output_dir = f'/mnt/nas5/suhyeon/projects/locmark/{self.exp_name}' # single image optimization
         self.output_dir = f'/mnt/nas5/suhyeon/projects/eval_spliceless/ours_convnext2' # NOTE: multi image optimization, exp_name
         # self.output_dir = "/mnt/nas5/suhyeon/projects/locmark/" # single image optimization
