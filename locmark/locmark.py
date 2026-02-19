8170406ec8b7a174acadb735c25992d7fcc10d30
SuhyeonHa <suhyeon.ha@kaist.ac.kr>
2026-02-19 22:26:42 +0900

    fix: Fix hinge loss in noise option

--- locmark/locmark.py
+++ locmark/locmark.py
@@ -310,7 +310,6 @@ class LocMark:
                     loss_m1 = torch.mean(F.relu(target_cosine - cos_sim_1))
                 else:
                     loss_m1 = torch.mean(1 - cos_sim_1)
-                loss_m1 = torch.mean(F.relu(target_cosine - cos_sim_1))
                 watermarked_image_1 = denorm_imagenet(watermarked_image_1)
 
             if is_hard and is_noise:
