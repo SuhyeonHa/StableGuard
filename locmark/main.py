1c97d542f11ed3422ad3458cb30beab9478cb798
SuhyeonHa <suhyeon.ha@kaist.ac.kr>
2026-02-19 14:06:27 +0900

    exp: Incraes epsilon as 4/255

--- locmark/main.py
+++ locmark/main.py
@@ -66,7 +66,7 @@ class Params:
         self.lambda_clean = 1.0
         self.lambda_noisy = 1.0
         self.feat_layer = 1
-        self.epsilon = 2/255
+        self.epsilon = 4/255
 
         # --- JND (Just Noticeable Difference) Parameters ---
         self.use_jnd = False  # Enable JND-based perceptual masking
