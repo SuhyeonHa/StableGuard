1612bb468a4195ba07acce4d0a5c8140420310d3
SuhyeonHa <suhyeon.ha@kaist.ac.kr>
2026-02-25 19:45:50 +0900

    exp: 100 images

--- analysis_layers.py
+++ analysis_layers.py
@@ -45,7 +45,7 @@ LAMBDA_P     = 0.025
 # 참고: ε=0.05 → 이론 max PSNR ≈ 26dB, ε=0.03 → ≈ 30dB
 EPSILON      = 32/255
 LAMBDA_CLEAN = 1.0
-NUM_IMAGES   = 10
+NUM_IMAGES   = 100
 CROP_RATIO   = 0.5
 
 TRAIN_DIR    = "/mnt/nas5/suhyeon/datasets/coco-2017/train2017"
