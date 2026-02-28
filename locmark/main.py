423bf179ab37e026350a28f124c6d1a400c15e47
SuhyeonHa <suhyeon.ha@kaist.ac.kr>
2026-03-01 05:49:17 +0900

    exp: Train with target 0.2

--- locmark/main.py
+++ locmark/main.py
@@ -40,7 +40,7 @@ class Params:
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         self.train_datasets = '/mnt/nas5/suhyeon/datasets/valAGE-Set'
         self.image_path = '/mnt/nas5/suhyeon/datasets/valAGE-Set/0034.png'
-        self.exp_name = 'hinge-hard-noise-target-0.15'
+        self.exp_name = 'hinge-hard-noise-target-0.2'
         # self.output_dir = f'/mnt/nas5/suhyeon/projects/locmark/{self.exp_name}' # single image optimization
         self.output_dir = f'/mnt/nas5/suhyeon/projects/locmark_table_1/ours' # NOTE: multi image optimization, exp_name
         # self.output_dir = "/mnt/nas5/suhyeon/projects/locmark/" # single image optimization
@@ -68,7 +68,7 @@ class Params:
         self.num_inference_steps = 100
         self.guidance_scale = 7.5
         self.temperature = 5.0
-        self.target_cossim = 0.15
+        self.target_cossim = 0.2
 
         # --- Optimization Parameters ---
         self.lr = 1.0
