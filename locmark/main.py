690b3b4eddb25c045bd019f1f0b15ea602146c58
SuhyeonHa <suhyeon.ha@kaist.ac.kr>
2026-03-08 17:37:41 +0900

    exp: Train with 200 steps

--- locmark/main.py
+++ locmark/main.py
@@ -40,7 +40,7 @@ class Params:
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         self.train_datasets = '/mnt/nas5/suhyeon/datasets/valAGE-Set'
         self.image_path = '/mnt/nas5/suhyeon/datasets/valAGE-Set/0049.png'
-        self.exp_name = 'supp-steps-100'
+        self.exp_name = 'supp-steps-200'
         # self.output_dir = f'/mnt/nas5/suhyeon/projects/locmark/{self.exp_name}' # single image optimization
         self.output_dir = f'/mnt/nas5/suhyeon/projects/locmark_table_1/ours' # NOTE: multi image optimization, exp_name
         # self.output_dir = "/mnt/nas5/suhyeon/projects/locmark/" # single image optimization
@@ -68,7 +68,7 @@ class Params:
 
         # --- Optimization Parameters ---
         self.lr = 1.0
-        self.steps = 100
+        self.steps = 200
         self.lambda_p = 0.1 #0.1 #0.05 #0.025
         self.lambda_i = 0.05 #0.05 #0.01 #0.005
         self.feat_layer = 1
