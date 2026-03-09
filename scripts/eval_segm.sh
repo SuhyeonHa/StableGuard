0b8bd9b15832d657565306cd15ea858aa8030399
SuhyeonHa <suhyeon.ha@kaist.ac.kr>
2026-03-09 15:48:34 +0900

    exp: Eval ours

--- scripts/eval_segm.sh
+++ scripts/eval_segm.sh
@@ -34,15 +34,28 @@ for mode in "${TAMPER_MODELS[@]}"; do
     #     segmentation_mask=True
 
     # stableguard
+    # python eval_AGE.py \
+    #     target_model=stableguard \
+    #     src_image_path=/mnt/nas5/suhyeon/datasets/valAGE-Set \
+    #     save_path=/mnt/nas5/suhyeon/projects/locmark_table_1/stableguard \
+    #     edit_model_name=sd-legacy/stable-diffusion-inpainting \
+    #     eval_size=256 \
+    #     start_idx=0 \
+    #     end_idx=100 \
+    #     tamper_mode=$mode \
+    #     segmentation_mask=True
+
+    # ours
     python eval_AGE.py \
-        target_model=stableguard \
+        target_model=ours \
         src_image_path=/mnt/nas5/suhyeon/datasets/valAGE-Set \
-        save_path=/mnt/nas5/suhyeon/projects/locmark_table_1/stableguard \
+        save_path=/mnt/nas5/suhyeon/projects/locmark_table_1/ours/hinge-hard-noise-500/20260226-045858 \
         edit_model_name=sd-legacy/stable-diffusion-inpainting \
         eval_size=256 \
         start_idx=0 \
         end_idx=100 \
         tamper_mode=$mode \
-        segmentation_mask=True
+        segmentation_mask=True \
+        use_refiner=False
 
 done
