4e4dc3edd6096de90bd0911144ff6680ca630210
SuhyeonHa <suhyeon.ha@kaist.ac.kr>
2026-05-04 21:35:42 +0900

    exp: Change guidance_scale of flux to 7

--- scripts/eval_rebuttal.sh
+++ scripts/eval_rebuttal.sh
@@ -2,7 +2,7 @@
 # Rebuttal evaluation: all target models vs. flux2 tamper mode
 
 NUM_IMG=100
-TAMPER_MODE=flux2
+TAMPER_MODE=flux
 DATASET=/mnt/nas5/suhyeon/datasets/valAGE-Set
 SAVE_BASE=/mnt/nas5/suhyeon/projects/apt_rebuttal
 
