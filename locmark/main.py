8ce841bc111a5054a1e66e84587b92362f1e6224
SuhyeonHa <suhyeon.ha@kaist.ac.kr>
2026-05-07 03:05:05 +0900

    exp: Try noise weak

--- locmark/main.py
+++ locmark/main.py
@@ -40,7 +40,7 @@ class Params:
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         self.train_datasets = '/mnt/nas5/suhyeon/datasets/valAGE-Set'
         self.image_path = '/mnt/nas5/suhyeon/datasets/valAGE-Set/0049.png'
-        self.exp_name = 'hnm-strong'
+        self.exp_name = 'noise-weak'
         # self.output_dir = f'/mnt/nas5/suhyeon/projects/locmark/{self.exp_name}' # single image optimization
         # self.output_dir = f'/mnt/nas5/suhyeon/projects/locmark_table_1/ours' # NOTE: multi image optimization, exp_name
         self.output_dir = f'/mnt/nas5/suhyeon/projects/apt_rebuttal/ours_ablation' # NOTE: multi image optimization, exp_name
@@ -83,7 +83,7 @@ class Params:
 
         # --- R2 Rebuttal Ablation: Loss / Noisy Branch ---
         self.hnm_type = "topk"           # "topk" | "focal"
-        self.hnm_k_percent = 0.20
+        self.hnm_k_percent = 0.10
         self.focal_gamma = 2.0
         self.noisy_type = "latent_masked"  # "latent_masked" | "vae_roundtrip" | "latent_full"
 
@@ -92,7 +92,7 @@ class Params:
         self.anchor_type = "submitted"
 
         # --- Robustness Parameters ---
-        self.eps0_std = [0.0, 0.25] # Latent noise sigma range
+        self.eps0_std = [0.0, 0.10] # Latent noise sigma range
         self.aug_noise_std = [0.0, 0.025] # Image-space Gaussian noise sigma range in [0,1]
         
         # --- Demo/Evaluation Parameters ---
